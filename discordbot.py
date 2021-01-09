dcc5aea5a73ca00ce7d20c23ee9b96d4d4e36c43
GinKa-0857 <77011380+GinKa-0857@users.noreply.github.com>
2021-01-09 20:58:55 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -19,12 +19,8 @@ async def on_member_join(member):
 # メッセージ受信時に動作する処理
 @client.event
 async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
         return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == '/neko':
-        await message.channel.send('にゃーん')
 
     if  'Laplace' in message.content:
         await message.channel.send(f'Hello,{message.author.display_name}.\nWhat do you need?')
