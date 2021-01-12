169c923e2d1541aca29a4daf8f676920b17abfe3
GinKa-0857 <77011380+GinKa-0857@users.noreply.github.com>
2021-01-12 20:50:04 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -30,7 +30,7 @@ async def on_message(message):
 #オペレーター申請システム
 @client.event
 async def on_message(message):
-    if message.guild is None and 'Apply' in message.content:
+    if message.guild is None and 'Apply' or 'apply' in message.content:
         await message.author.send('Ok.')
 
 
